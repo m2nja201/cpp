08243c2288c07cb0b73470e8314b6b52bc741a94
m2nja201 <m2nja201@gmail.com>
2022-03-06 23:54:18 +0900

    0306 c++ study

---
+++ after0224/after0224/lost.cpp
@@ -0,0 +1,18 @@
+// ภาพ๎น๖ธฐ ฐýศฃ(1541)
+// 03.06
+//
+
+#include <iostream>
+using namespace std;
+#define endl "\n"
+
+string equation;
+
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cout.tie(0); cin.tie(0);
+
+	cin >> equation;
+
+}
