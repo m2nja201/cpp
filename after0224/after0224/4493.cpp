a5385a14e6c571fb769b49b8eba293ebda672a17
m2nja201 <m2nja201@gmail.com>
2022-06-28 15:32:25 +0900

    4493

---
+++ after0224/after0224/4493.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int play(char a, char b){
+    if (a == b) return 0;
+    if (a == 'R'){
+        if (b == 'P') return 1; // player 2 win
+        else if (b == 'S') return -1; // player 1 win
+    }
+    else if (a == 'P'){
+        if (b == 'R') return -1;
+        else if (b == 's') return 1;
+    }
+    else if (a == 'S'){
+        if (b == 'R') return 1;
+        else if (b == 'P') return -1;
+    }
+    return 2; //err
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int total;
+    cin >> total;
+    for (int i = 0; i < total; i++){
+        int n;
+        int win1 = 0, win2 = 0;
+        cin >> n;
+        for (int j = 0; j < n; j++){
+            char p1, p2;
+            cin >> p1 >> p2;
+            if (play(p1, p2) == 1) win2++;
+            else if (play(p1, p2) == -1) win1++;
+        }
+        if (win2 > win1) cout << "Player 2\n";
+        else if (win2 < win1) cout << "Player 1\n";
+        else cout << "TIE\n";
+    }
+    return 0;
+}
